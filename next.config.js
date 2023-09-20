0230960bd07b5fbf622bc753f700dad808aa7a72
umidullo <suyunovumidullo@gmail.com>
2023-09-20 17:10:21 +0500

    update: framework

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
   i18n,
   images: {
     domains: ["prodesignstudio.uz"],
